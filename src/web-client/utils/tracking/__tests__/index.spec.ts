d9cdd83e24130287f34f404b7e00b48fea433878
Danny <danjmurphy92@gmail.com>
2020-01-18 18:24:41 +0000

    Using GAs own flag for enabled/disabled tracking

--- src/web-client/utils/tracking/__tests__/index.spec.ts
+++ src/web-client/utils/tracking/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { trackPictureImpression, initGa, pageView } from '../'
+import { trackPictureImpression, initGa, pageView, disableTracking } from '../index'
 
 describe('Tracking', () => {
   const gtagSpy = jest.fn()
@@ -6,14 +6,72 @@ describe('Tracking', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
+
   beforeEach(() => {
     window.gtag = gtagSpy
-    initGa()
-    jest.clearAllMocks()
+  })
+
+  describe('GIVEN initial state', () => {
+    describe('GIVEN events triggered when gtag not initialized', () => {
+      beforeEach(() => {
+        pageView()
+        trackPictureImpression('test')
+      })
+
+      it('THEN does not invoke ga', () => {
+        expect(gtagSpy).not.toBeCalled()
+      })
+
+      describe('WHEN ga initialised', () => {
+        beforeEach(() => {
+          initGa()
+        })
+
+        it('THEN initializes GA correctly', () => {
+          expect(gtagSpy).toBeCalledWith('config', 'UA-155099216-1')
+        })
+
+        it('THEN processes the previous events', () => {
+          expect(gtagSpy).toHaveBeenCalledWith('pageview')
+          expect(gtagSpy).toHaveBeenCalledWith('event', 'impression', {
+            event_category: 'picture',
+            event_label: 'test',
+          })
+        })
+
+        it('THEN sets window disable tracking to false', () => {
+          expect(window['ga-disable-UA-155099216-1']).toBe(false)
+        })
+
+        describe('WHEN disable tracking', () => {
+          beforeEach(() => {
+            disableTracking()
+          })
+
+          it('THEN sets window disable tracking to true', () => {
+            expect(window['ga-disable-UA-155099216-1']).toBe(true)
+          })
+
+          describe('WHEN an event is triggered', () => {
+            beforeEach(() => {
+              jest.clearAllMocks()
+              pageView()
+              trackPictureImpression('test')
+            })
+
+            it('THEN does not track the action', () => {
+              expect(gtagSpy).not.toBeCalled()
+            })
+          })
+        })
+      })
+    })
   })
 
   describe('Impression tracking', () => {
     beforeEach(() => {
+      initGa()
+      jest.clearAllMocks()
       trackPictureImpression('testlabel')
     })
 
@@ -26,36 +84,10 @@ describe('Tracking', () => {
     })
   })
 
-  describe('Initialize google analytics', () => {
-    describe('GIVEN events triggered when gtag not on window', () => {
-      beforeEach(() => {
-        pageView()
-        trackPictureImpression('test')
-      })
-
-      describe('WHEN ga initialised', () => {
-        beforeEach(() => {
-          initGa()
-        })
-
-        it('THEN initializes GA', () => {
-          expect(gtagSpy).toBeCalledTimes(4)
-          expect(gtagSpy).toBeCalledWith('config', 'UA-155099216-1')
-          expect(gtagSpy).toHaveBeenCalledWith('pageview')
-        })
-
-        it('THEN processes the previous events', () => {
-          expect(gtagSpy).toHaveBeenCalledWith('event', 'impression', {
-            event_category: 'picture',
-            event_label: 'test',
-          })
-        })
-      })
-    })
-  })
-
   describe('pageview', () => {
     beforeEach(() => {
+      initGa()
+      jest.clearAllMocks()
       pageView()
     })
 
--- src/web-client/utils/tracking/index.ts
+++ src/web-client/utils/tracking/index.ts
@@ -1,5 +1,16 @@
-let gaInitialized = false
 const queue: any[] = []
+
+export const initGa = () => {
+  window['ga-disable-UA-155099216-1'] = false
+  window.gtag('js', new Date())
+
+  window.gtag('config', 'UA-155099216-1')
+  processItemsInQueue()
+}
+
+export const disableTracking = () => {
+  window['ga-disable-UA-155099216-1'] = true
+}
 
 export const trackPictureImpression = (label: string) => {
   pushToGa('event', 'impression', {
@@ -8,27 +19,20 @@ export const trackPictureImpression = (label: string) => {
   })
 }
 
-export const initGa = () => {
-  gaInitialized = true
-  window.gtag('js', new Date())
-
-  window.gtag('config', 'UA-155099216-1')
-  processItemsInQueue()
-}
-
 export const pageView = () => {
   pushToGa('pageview')
 }
 
 const processItemsInQueue = () => {
-  queue.forEach((item) => {
-    pushToGa(item)
-  })
+  while (queue.length > 0) {
+    pushToGa(queue.shift())
+  }
 }
 
 const pushToGa = (...args: any[]) => {
-  if (!gaInitialized) {
+  if (window['ga-disable-UA-155099216-1'] === undefined || window['ga-disable-UA-155099216-1'] === true) {
     queue.push(args)
+  } else {
+    window.gtag(...args)
   }
-  window.gtag(...args)
 }
